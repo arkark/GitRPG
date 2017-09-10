f873a349c925e3954b630f019d7b9cd15a36d59c
eision <eision848877@gmail.com>
2017-09-10 16:28:19 +0900

    wav complete

--- src/git_branch.py
+++ src/git_branch.py
@@ -5,5 +5,8 @@ from src.se_manager import play
 from src.util import getColorString
 from src.util import getColorText
 
-def branch(cmd, se_path):
-    play(se_path + "/ta/流れ星01.mp3")
+def branch(args):
+    args.se_manager.play_wav("metro")
+
+#def branch(cmd, se_path):
+    #play(se_path + "/ta/流れ星01.mp3)
--- src/git_log.py
+++ src/git_log.py
@@ -6,15 +6,5 @@ from src.se_manager import play
 from src.util import getColorString
 from src.util import getColorText
 import os
-
-def log(cmd, se_path):
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    play(se_path + "/ta/ビヨォン.mp3")
-    f = open(os.path.dirname(os.path.abspath(__file__)) + '/../text.txt', 'r')
-    string = f.read()
-    string = getColorString(string, 5, 30, 41)
-    return string
+def log(args):
+    args.se_manager.play_wav("goukaku")
