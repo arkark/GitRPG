3d0b0e3eb3623af7f6ac9d98843d751e693e3a40
eision <eision848877@gmail.com>
2017-09-10 20:05:01 +0900

    gamestartupUI

--- src/git_init.py
+++ src/git_init.py
@@ -9,6 +9,10 @@ import os
 
 def init(args):
     args.se_manager.play_wav("hora")
+    with open(os.path.dirname(os.path.abspath(__file__))+ "/../ascii_art/git_rpg.txt", "r") as file:
+    	string1 = file.read()
+    	string1 = getColorString(string1, 1, 37, 44)
     with open(os.path.dirname(os.path.abspath(__file__))+ "/../ascii_art/game_start.txt", "r") as file:
-    	string = file.read()
-    	return string
+    	string2 = file.read()
+    	string2 = getColorString(string2, 5, 37, 40)
+    	return string1 + '\n' + string2
